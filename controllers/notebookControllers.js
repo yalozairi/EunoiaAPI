20d0e3897f7f83650be86100b2245c30181c3ae6
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-07-26 19:56:38 +0300

    finalized middlewares and added param middleware

--- controllers/notebookControllers.js
+++ controllers/notebookControllers.js
@@ -1,4 +1,14 @@
+//Data
 const { Notebook } = require("../db/models");
+
+exports.fetchNotebook = async (notebookId, next) => {
+  try {
+    notebook = await Notebook.findByPk(notebookId);
+    return notebook;
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.notebookList = async (req, res, next) => {
   try {
@@ -13,7 +23,7 @@ exports.notebookList = async (req, res, next) => {
 
 exports.notebookCreate = async (req, res, next) => {
   try {
-    const newNotebook = await Notebook.crate(req.body);
+    const newNotebook = await Notebook.create(req.body);
     res.status(201).json(newNotebook);
   } catch (error) {
     next(error);
@@ -22,16 +32,8 @@ exports.notebookCreate = async (req, res, next) => {
 
 exports.notebookUpdate = async (req, res, next) => {
   try {
-    const { notebookId } = req.params;
-    const foundNotebook = await Notebook.findByPk(notebookId);
-    if (foundNotebook) {
-      await foundNotebook.update(req.body);
-      res.status(204).end();
-    } else {
-      const err = new Error("Notebook Not Found");
-      err.status = 404;
-      next(err);
-    }
+    await req.notebook.update(req.body);
+    res.status(204).end();
   } catch (error) {
     next(error);
   }
@@ -39,16 +41,8 @@ exports.notebookUpdate = async (req, res, next) => {
 
 exports.notebookDelete = async (req, res, next) => {
   try {
-    const { notebookId } = req.params;
-    const foundNotebook = await Notebook.findByPk(notebookId);
-    if (foundNotebook) {
-      await foundNotebook.destroy();
-      res.status(204).end();
-    } else {
-      const err = new Error("Notebook Not Found");
-      err.status = 404;
-      next(err);
-    }
+    await req.notebook.destroy();
+    res.status(204).end();
   } catch (error) {
     next(error);
   }
--- routes/notebooks.js
+++ routes/notebooks.js
@@ -6,9 +6,22 @@ const {
   notebookList,
   notebookUpdate,
   notebookDelete,
+  fetchNotebook,
 } = require("../controllers/notebookControllers");
 
 const router = express.Router();
+
+router.param("notebookId", async (req, res, next, notebookId) => {
+  const notebook = await fetchNotebook(notebookId, next);
+  if (notebook) {
+    req.notebook = notebook;
+    next();
+  } else {
+    const err = new Error("Notebook Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
 
 //List
 router.get("/", notebookList);
