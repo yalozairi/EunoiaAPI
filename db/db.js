cf2456746d5a0d61a59ed322001fe80be0493e72
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-16 00:31:37 +0300

    fix db

--- db/db.js
+++ db/db.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require("sequelize");
+const { sequelize } = require("./models/User");
 
-const db = new Sequelize({
-  username: "postgres",
-  password: "3982",
-  database: "eunoiadb",
-  dialect: "postgres",
-  host: "localhost",
-  logging: false,
-});
+const db = process.env.DATABASE_URL
+  ? new sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+    })
+  : new Sequelize({
+      username: "postgres",
+      password: "3982",
+      database: "eunoiadb",
+      dialect: "postgres",
+      host: "localhost",
+      logging: false,
+    });
 
 module.exports = db;
